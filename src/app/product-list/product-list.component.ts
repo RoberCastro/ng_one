bcb5ca64ad37ae62328b73cd90c377d568c30767
robercastro <robertoicastro@gmail.com>
2017-03-16 13:04:38 +0100

    Data binding et & pipes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 interface IProduct{
   id : number,
-  image : string,
+  images : string[],
   name : string,
-  note : number
+  note : number,
+  index : number,
+  esVisible: boolean
 }
 
 @Component({
@@ -17,34 +19,47 @@ export class ProductListComponent implements OnInit {
   pageTitle: String = 'Mes produits son contents';
   products: IProduct [] = [];
 
+
   constructor() {
 
     this.products.push({
       id : 1,
-      image : 'https://www.w3schools.com/css/img_fjords.jpg',
+      images : [
+        'https://www.w3schools.com/css/img_fjords.jpg',
+        'https://d1ez3020z2uu9b.cloudfront.net/imagecache/blog-photos/2245.jpg',
+        'https://image.yachtcharterfleet.com/yacht-reviews/rodas-beach-las-islas-cies-galicia-spain-large.jpg?k=676e&w=900&h=800&q=75&o=i&image_id=30246'
+      ],
       name : 'test',
       note : 6,
+      index : 0,
+      esVisible: true,
 
     });
     this.products.push({
       id : 2,
-      image : 'https://www.w3schools.com/css/img_fjords.jpg',
-      name : 'internet image',
-      note : 5,
-
-    });
-
-    this.products.push({
-      id : 3,
-      image : 'https://www.w3schools.com/css/img_fjords.jpg',
-      name : 'mon image',
+      images : [],
+      name : ' 2. test',
       note : 3,
+      index : 0,
+      esVisible: true,
 
     });
+
   }
 
   ngOnInit() {
 
   }
+
+  changerPhoto(product : IProduct){
+    //Le modulo de la longueur du tableau ça permet que quand longueur du tableau
+    //soit égal à l'index le modulo soit zéro ainsi que le nouveau index
+    product.index = (product.index + 1) % product.images.length;
+  }
+
+  showHide(product : IProduct){
+    product.esVisible = !product.esVisible;
+  }
+
 
 }
